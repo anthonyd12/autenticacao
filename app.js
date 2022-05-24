ae0d07f539dd179fcc6cbdce9b3c1a74027fa08c
Anthony Daniel <93259818+anthonyd12@users.noreply.github.com>
2022-05-24 15:37:36 -0300

    verificar user

--- app.js
+++ app.js
@@ -7,20 +7,63 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+
+
+
+
 //config json response
-app.use(express.json())
+    app.use(express.json())
+
+
+
+
 
 //models
-const User = require('./models/User')
+    const User = require('./models/User')
+
+
+
+
 
 //Open Route - public
-app.get('/',(req,res)=> {
-    res.status(200).json({ msg: "Welcome the new API" })
-})
+    app.get('/',(req,res)=> {
+        res.status(200).json({ msg: "Welcome the new API" })
+    })
+
+
+
+
+
+//private - route
+    app.get("/user/:id", async(req, res) => {
+        const id = req.params.id
+
+
+        //check if user exists
+        const user = await User.findById(id, '-passorwd')
+        if(!user){
+            return res.status(404).json({msg: "User not found"})
+        }
+        res.status(200).json({ user })
+
+    })
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers('authorization')
+    const token = authHeader && authHeader.split("")[1]
+
+    if(!token) {
+        return res.status(401).json({msg: "Acess negate"})
+    }
+}
+
+
 
 //register user
-app.post('/auth/register', async(req, res) => {
-    const { name, email, password, confirmpassword } = req.body
+    app.post('/auth/register', async(req, res) => {
+        const { name, email, password, confirmpassword } = req.body
+
+
 
     //validations
     if(!name) {
@@ -36,42 +79,109 @@ app.post('/auth/register', async(req, res) => {
         return res.status(422).json({ msg: 'Passwords do not match'})
     }
 
+
+
     // check if user exists
     const userExists = await User.findOne({ email: email })
 
     if (userExists){
         return res.status(422).json({msg: 'E-mail already registered'})
     }
+
+
+
     
 //create password
     const salt = await bcrypt.genSalt(12)
     const passwordHash = await bcrypt.hash(password, salt)
 
+
+
+
+
 //create user
     const user = new User({
         name,
         email,
-        password
+        password: passwordHash,
 })
 
-try {
+    try {
 
-} catch(error){
-    res.status(500).json({msg: "Erro no server"})
-}
+        await user.save()
+        res.status(201).json({msg: "User created successfully"})
+
+    } catch(error) {
+        res.status(500).json({msg: "Erro no server"})
+    }
 })
+
+
+
+
+
+//login
+    app.post("/auth/login", async (req, res) => {
+        const {email, password} = req.body
+
+
+
+    //validations
+        if(!email) {
+            return res.status(422).json({msg: 'Put an email'})
+        }
+        if(!password) {
+            return res.status(422).json({msg: 'Put a password'})
+    }
+
+
+
+//check if user exist
+    const user = await User.findOne({ email: email })
+
+    if (!user){
+        return res.status(404).json({msg: 'user not found'})
+    }
+
+
+
+
+//check if passowrd match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(422).json({msg: 'password invalid'})
+    }
+
+    try{
+        const secret = process.env.SECRET
+
+        const token = jwt.sign(
+        {
+            id: user._id,
+        },
+        secret,
+        )
+
+        res.status(200).json({msg: "authentication completed", token})
+    } catch(err){
+        res.status(500).json({msg: "Erro no server"})
+    }
+})
+
+
 
 
 
 //credenciais
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+    const dbUser = process.env.DB_USER
+    const dbPassword = process.env.DB_PASS
 
-mongoose.connect(
-    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.5eijq.mongodb.net/?retryWrites=true&w=majority`)
-    .then(()=> {
-    app.listen(3000)
-    console.log("conected to api")
-})
-.catch((err)=> console.log(err))
+    mongoose.connect(
+        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.5eijq.mongodb.net/?retryWrites=true&w=majority`)
+        .then(()=> {
+        app.listen(3000)
+        console.log("conected to api")
+    })
+    .catch((err)=> console.log(err))
 
